823c95bdbc3c32060c264ed516be0c1b4424fbd4
ch-aimaa <84049531+ch-aimaa@users.noreply.github.com>
2021-07-05 15:09:31 +0100

    Update 0-subs.py

--- 0x06-api_advanced/0-subs.py
+++ 0x06-api_advanced/0-subs.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-import sys
 
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+"""[SUBREDDIT]
+"""
+
+
+def number_of_subscribers(subreddit):
+    import requests
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    req = requests.get(url,
+                       headers={'User-Agent': 'Omar'},
+                       allow_redirects=False)
+
+    if req.status_code != 200:
+        return 0
+    return req.json()['data']['subscribers']
